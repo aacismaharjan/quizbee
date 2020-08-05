7fe27bdbe480bf9d24687e34521eab2a759d53c9
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-08-06 03:47:20 +0545

    Updates

---
+++ precache-manifest.dce76990eda62bd121d3aa43d4183610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e589a660d5ba2d58918f3c995ea86e5",
+    "url": "/quizbee/index.html"
+  },
+  {
+    "revision": "81cb2806aa358fbad9ba",
+    "url": "/quizbee/static/css/main.6e908b43.chunk.css"
+  },
+  {
+    "revision": "9371470ff225a6858de8",
+    "url": "/quizbee/static/js/2.c31eb7ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizbee/static/js/2.c31eb7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81cb2806aa358fbad9ba",
+    "url": "/quizbee/static/js/main.6601a34b.chunk.js"
+  },
+  {
+    "revision": "4ea183013ea8e32ea31c",
+    "url": "/quizbee/static/js/runtime-main.c664f4b9.js"
+  }
+]);
